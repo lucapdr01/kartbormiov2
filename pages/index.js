3983cb74f3ba60e35bae19da6046ab659fcfce60
lucapedr01 <p2001luca@gmail.com>
2022-07-08 23:23:51 +0200

    fine tuning

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,11 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Kartdromo Bormio</title>
-        <meta name="description" content="Sito Ufficiale del Kartdromo di Bormio" />
+        <title>Kartdromo Bormio | Go kart</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="description" content="Sito Ufficiale del Kartdromo di Bormio. Luogo ideale per i giovani e per il divertimento per chi si trova in Alta Valle nelle vicinaze di Bormio"/>
+        <meta name="keywords" content="Kartdromo, Bormio, Sport, Kart, Alta Valtellina, divertimento, go kart, giovani, divertimento bormio, cosa fare a Bormio"/>
+        <meta http-equiv="author" content="Luca Pedranzini" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
