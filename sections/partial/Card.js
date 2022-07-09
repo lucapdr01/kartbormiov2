a1ee525d3015508ad2fc3225358b8ffa86605fb2
lucapedr01 <p2001luca@gmail.com>
2022-07-09 12:04:19 +0200

    loaders

--- sections/partial/Card.js
+++ sections/partial/Card.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import Image from "next/image";
+import { Rings } from  'react-loader-spinner'
 
 function Card(props) {
+    const [loaded, setLoaded] = useState(false);
+
+
+    useEffect(()=>{
+        console.log(loaded);
+    },[loaded]);
     
     return (
         <>
         <div className="p-2 my-5 splash-card-bg-ext rounded-3xl">
+            
+            
+            {!loaded ?
+            <>
+            <div className="mx-auto tex-center grid justify-items-center ">
+            <Rings className="mx-auto text-center "
+            color="#00BFFF" height={100} width={100} />
+            </div> 
+            </> :
+            <>
+            </>
+            }
             <div className="h-72 relative rounded-3xl">
-            <Image className="rounded-3xl mx-auto" src={props.asset} alt=""  layout="fill" objectFit="cover"/>
+            <Image className="rounded-3xl mx-auto" src={props.asset} alt=""  layout="fill" objectFit="cover" onLoad={()=>{setLoaded(true) }}/>
             </div>
             <div className='splash-card-bg-int rounded-xl px-4 py-4 my-4'>
             <h1 className="h1 text-left pt-4 text-2xl lg:text-3xl mb-4 font-red-hat-display" data-aos="fade-down">
